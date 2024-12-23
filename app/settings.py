a9189d26954802b27cb645a9d13e82472d5b0dc0
Elicarlos <elicarlosantos_@hotmail.com>
2024-12-23 04:09:59 -0300

    preparando para deploy

--- app/settings.py
+++ app/settings.py
@@ -78,10 +78,21 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carros',
+        'USER': 'postgres',
+        'PASSWORD': 'djangomaster',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
@@ -116,7 +127,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-API_OPEN_AI = config('API_OPEN_AI')
+# API_OPEN_AI = config('API_OPEN_AI')
 
 
 # Static files (CSS, JavaScript, Images)
