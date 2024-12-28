bdc9c798a5726b745b4a0e41ba6c522f28d7964d
Elicarlos <elicarlosantos_@hotmail.com>
2024-12-27 21:39:14 -0300

    feito atualização uwsgi_params

--- app/settings.py
+++ app/settings.py
@@ -136,6 +136,8 @@ API_OPEN_AI=""
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
